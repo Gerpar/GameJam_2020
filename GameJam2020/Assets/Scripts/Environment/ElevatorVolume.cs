2a29073d028fa93a07b6258c4b2ee1894dd9ed34
Gerpar <Gerpar.gerps@gmail.com>
2020-02-01 11:29:57 -0500

    Created Elevator Volume script

---
+++ GameJam2020/Assets/Scripts/Environment/ElevatorVolume.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorVolume : MonoBehaviour
+{
+    [SerializeField] float verticalSpeed;
+    public bool elevatorIsOn = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player") && elevatorIsOn)
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(rb.velocity.x, verticalSpeed, rb.velocity.z);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && elevatorIsOn)
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+        }
+    }
+}
