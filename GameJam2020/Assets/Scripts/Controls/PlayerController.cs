c2eb1f74977508bf6825c7d5a8b5f1ff56c6f85d
Gerpar <Gerpar.gerps@gmail.com>
2020-02-01 01:37:05 -0500

    Worked on particles

--- GameJam2020/Assets/Scripts/Controls/PlayerController.cs
+++ GameJam2020/Assets/Scripts/Controls/PlayerController.cs
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
 
         rb.velocity = new Vector3(0, rb.velocity.y, 0);
 
-        if(timeToNextFootstep < Time.time && inputVector != Vector3.zero)
+        if(timeToNextFootstep < Time.time && inputVector != Vector3.zero && canJump)
         {
             if(firstFootstep)
             {
