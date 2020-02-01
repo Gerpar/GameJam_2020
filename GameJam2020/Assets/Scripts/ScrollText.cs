3e640db701c34b3140323e2d7a53550d386c103f
dnguyen32 <60530281+dnguyen32@users.noreply.github.com>
2020-02-01 00:04:53 -0500

    Started a credits scene

---
+++ GameJam2020/Assets/Scripts/ScrollText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollText : MonoBehaviour
+{
+    public float scrollSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < (Screen.height / 2))
+            transform.Translate(Vector3.up * Time.deltaTime * scrollSpeed);
+    }
+}
