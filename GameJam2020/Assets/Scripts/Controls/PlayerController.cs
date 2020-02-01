248aef0ee4c805863f7e1f55337ea5644cf26c3d
Gerpar <Gerpar.gerps@gmail.com>
2020-02-01 00:11:20 -0500

    Created flashlight and movement audio

--- GameJam2020/Assets/Scripts/Controls/PlayerController.cs
+++ GameJam2020/Assets/Scripts/Controls/PlayerController.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Speed Settings")]
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpVelocity;
-    [SerializeField] float cameraSensitivity;
     [SerializeField] float sprintMulti;
+
+    [Header("Camera Settings")]
+    [SerializeField] float cameraSensitivity;
     [SerializeField] Camera camera;
     [SerializeField] float minCamLimit, maxCamLimit;
+
+    [Header("Footstep Settings")]
+    [SerializeField] AudioClip[] footstepClips;
+    [SerializeField] float footstepDelay;
 
     Vector3 inputVector;
     Rigidbody rb;
     bool canJump = true;
     Vector2 camRot;
+    float timeToNextFootstep; // Keeps track of when the next footstep can play
+    AudioSource src;
+    bool firstFootstep = true;  // Keeps track of which footstep sound to play
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        src = GetComponent<AudioSource>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -37,17 +48,38 @@ public class PlayerController : MonoBehaviour
         inputVector.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         inputVector.z = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 
-        if(Input.GetButton("Sprint"))
+        if(Input.GetButton("Sprint"))   // Multiplies the speed if the player is sprinting
         {
             inputVector *= sprintMulti;
         }
 
-        Debug.Log(inputVector.z);
-
-        rb.transform.position += transform.forward * inputVector.z ;
+        rb.transform.position += transform.forward * inputVector.z;
         rb.transform.position += transform.right * inputVector.x;
 
         rb.velocity = new Vector3(0, rb.velocity.y, 0);
+
+        if(timeToNextFootstep < Time.time && inputVector != Vector3.zero)
+        {
+            if(firstFootstep)
+            {
+                src.PlayOneShot(footstepClips[0]);
+            }
+            else
+            {
+                src.PlayOneShot(footstepClips[1]);
+            }
+
+            firstFootstep = !firstFootstep;
+
+            if (Input.GetButton("Sprint"))   // Multiplies the speed if the player is sprinting
+            {
+                timeToNextFootstep = Time.time + (footstepDelay / sprintMulti);
+            }
+            else
+            {
+                timeToNextFootstep = Time.time + footstepDelay;
+            }
+        }
     }
 
     void Jumping()
