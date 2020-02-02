e3c8b6b2a1ce3919fedf05f5418fdcab359f96de
Gerpar <Gerpar.gerps@gmail.com>
2020-02-02 10:22:49 -0500

    Imported AI

---
+++ GameJam2020/Assets/Scripts/AI/PlayerDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public int detectionLayer = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            detectionLayer++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            detectionLayer--;
+        }
+    }
+}
