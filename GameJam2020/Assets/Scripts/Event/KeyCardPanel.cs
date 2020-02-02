264cf46b1bc56afa1e76e590542c3ae86e4861e4
Nwihim <60072929+Nwihim@users.noreply.github.com>
2020-02-01 19:58:43 -0500

    Added Keycard and Wheel Prefab, Setup KeyPanel script

---
+++ GameJam2020/Assets/Scripts/Event/KeyCardPanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardPanel : MonoBehaviour
+{
+    public KeyCardPickUp playerKeyCard;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") && playerKeyCard.hasKeyCard)
+        {
+            //Play swipe animation
+
+            //Open door???
+
+        }
+    }
+}
